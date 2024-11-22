bcf470be564def38956a28c6d32edb5e91894448
Abuvakhobov Isroiljon <65494864+Isroiljoncyber@users.noreply.github.com>
2024-11-22 22:46:48 +0500

    FIX: - flutter_gl changed to git ref; - example removed to prevent errors

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,14 @@
-// import 'package:example/multi_views.dart';
 import 'package:flutter/material.dart';
-import 'example_app.dart';
 
 void main() {
   runApp(const ExampleApp());
-  // runApp(MaterialApp(home: multi_views(fileName: 'multi_views'),));
-  // runApp(MaterialApp(home: misc_controls_arcball(fileName: 'misc_controls_arcball'),));
-  // runApp(MaterialApp(home: webgl_loader_gltf_test(fileName: 'webgl_loader_gltf_test'),));
+}
+
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
